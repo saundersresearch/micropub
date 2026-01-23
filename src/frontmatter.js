dbfdff9af8e301eee5743f21b471c526be6fe10f
saundersresearch <adam.m.saunders@vanderbilt.edu>
2026-01-22 21:09:28 -0600

    Update published/deleted mapping

--- src/frontmatter.js
+++ src/frontmatter.js
@@ -18,19 +18,21 @@ export const translateProperties = (data, reverse) => {
 		properties[dictionary[key] || key] = value
 	}
 
-    // Want to map deleted: true --> published: false and vise versa
-    if (!reverse) {
-        if (Array.isArray(properties.deleted) && properties.deleted.includes(true)) {
-        properties.published = false
-        delete properties.deleted
-        }
-    }
-    if (reverse) {
-        if (properties.published === false) {
-        properties.deleted = [true]
-        delete properties.published
-        }
-    }
+	// Flip published/deleted boolean values
+	if (!reverse) {
+		if (Array.isArray(properties.deleted) && properties.deleted.includes(true)) {
+		properties.published = [false]
+		delete properties.deleted
+		}
+	}
+
+	if (reverse) {
+		if (Array.isArray(properties.published) && properties.published.includes(false)) {
+		properties.deleted = [true]
+		delete properties.published
+		}
+	}
+
 	return properties
 }
 
