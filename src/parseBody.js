afa4c469904a93fbb5b23e613fce471438ec8122
saundersresearch <adam.m.saunders@vanderbilt.edu>
2026-01-29 10:49:12 -0600

    Update parseBody.js

--- src/parseBody.js
+++ src/parseBody.js
@@ -16,15 +16,11 @@ const parseMultipart = async (req) => {
 	const headers = Object.fromEntries(req.headers.entries())
 
 	const addProperty = (store, name, value) => {
-		const isArray = name.endsWith('[]')
-		const key = isArray ? name.slice(0, -2) : name
-		if (isArray) {
-			if (!store[key]) store[key] = []
-			store[key].push(value)
-		} else {
-			store[key] = value
-		}
-	}
+		if (!name) return; // <-- guard against undefined
+		const key = name.endsWith('[]') ? name.slice(0, -2) : name;
+		if (!store[key]) store[key] = [];
+		store[key].push(value);
+	};
 
 	return new Promise((resolve, reject) => {
 		const bb = Busboy({ headers })
